5898974b439d5f99d8a0dfa2b55247ae31972d11
23suca38 <23suca38@tcarts.in>
2026-03-20 19:41:09 +0530

    Cleaned repo - removed backend files

--- leavemate/src/main/java/com/example/leavemate/repository/EmployeeLeaveRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.leavemate.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.leavemate.model.EmployeeLeave;
-
-public interface EmployeeLeaveRepository extends JpaRepository<EmployeeLeave, Long> {
-}
