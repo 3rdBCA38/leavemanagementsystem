5e3455a35ba4d3cd9ed7281b0969da9d961c32d5
23suca38 <23suca38@tcarts.in>
2026-03-21 15:37:19 +0530

    Fixed controller imports

--- leavemate/src/main/java/com/example/leavemate/controller/CorsConfig.java
+++ leavemate/src/main/java/com/example/leavemate/controller/CorsConfig.java
@@ -1,8 +1,9 @@
-package com.example.leavemate.conf;
+package com.example.leavemate.controller;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
